95be4289e46b921833b1967ef835f82e138c6c38
noramohamsa <nora.mohamed231@gmail.com>
2023-04-10 19:53:17 +0200

    "first modify"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 function App() {
   const [countOne, setCountOne] = useState(0)
@@ -11,16 +11,19 @@ function App() {
   const handelCountTwo = () => {
     setCountTwo(countTwo + 1)
   }
-  const isEven = () => {
-    return countOne % 2 === 0
-  }
+  const isEven = useMemo(() => {
+    let i = 0;
+    while (i < 20000000) i++;
+    return countOne % 2 === 0;
+  }, [countOne])
+
   const isEvenTwo = () => {
     return countTwo % 2 === 0
   }
   return (
     <div className="App">
       <button onClick={handelCountOne}>countOne -{countOne}</button>
-      {isEven() ? "EVEN NUMBER" : 'ODD NUMBER'}<br />
+      <div>{isEven ? "EVEN NUMBER" : 'ODD NUMBER'}</div>   <br />
       <button onClick={handelCountTwo}>countOne -{countTwo}</button>
       {isEvenTwo() ? "EVEN NUMBER" : 'ODD NUMBER'}
     </div>
